8764e0f715d284e3eda85f9122f1c6d50d7c1d52
Maycon Brito <maycon@localhost.localdomain>
2017-01-18 22:12:48 -0200

    Added /var/www/ Link From web Folder

--- script_home_Maycon.sh
+++ script_home_Maycon.sh
@@ -5,6 +5,7 @@ ln -s /media/Data/Games Games
 ln -s /media/Data/Musicas Music
 ln -s /media/Data/Softwares Softwares
 ln -s /media/Data/Softwares/Desenvolvimento/composer ~/composer
+ln -sfn /media/Data/Google\ Drive/Documents/Desenvolvimento/web /var/www
 ln -s /media/Data/Videos Videos
 ln -s /media/Data/Google\ Drive/Documents Documents
 ln -s /media/Data/Google\ Drive/Documents/Desenvolvimento/web web
