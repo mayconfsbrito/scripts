0cbb286ed10cc8a3e1d63d37f7abe3a13dfc975e
Maycon Brito <maycon.brito@bioextratus.rede.bioextratus>
2017-11-22 17:53:24 -0200

    Created Script For Selenium Server Install
    Install Selenium and configure firefox (gecko) and chrome webdrivers.

--- init-selenium-server.sh
+++ init-selenium-server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GECKO_DRIVER_VERSION="0.19.1"
+CHROME_DRIVER_VERSION="2.33"
+
 source "$DIR/assets/is_root"
 
 FILE="/usr/local/bin/selenium-server-standalone.jar"
@@ -10,5 +13,29 @@ if [ ! -f ${FILE} ]; then
     wget https://goo.gl/3aWDMc -O selenium-server-standalone.jar
     chmod 777 ${FILE}
 fi
-java -jar ${FILE}
+
+# https://github.com/mozilla/geckodriver/releases
+FILE_GECKO="/usr/local/lib/geckodriver"
+if [ ! -f ${FILE_GECKO} ]; then
+	isRoot 1
+	cd /usr/local/lib/
+	wget  https://github.com/mozilla/geckodriver/releases/download/${GECKO_DRIVER_VERSION}/geckodriver-${GECKO_DRIVER_VERSION}-linux64.tar.gz -O geckodriver.tar.gz
+	tar -zxvf geckodriver.tar.gz
+	chmod 777 ${FILE_GECKO}
+	rm -rf geckodriver.tar.gz
+fi
+
+# https://sites.google.com/a/chromium.org/chromedriver/downloads
+FILE_CHROME="/usr/local/lib/chromedriver"
+if [ ! -f ${FILE_CHROME} ]; then
+	isRoot 1
+	cd /usr/local/lib/
+	wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip -O chromedriver.zip
+	unzip chromedriver.zip -d /usr/local/lib
+	chmod 777 ${FILE_CHROME}
+	rm -rf chromedriver.zip
+fi
+
+echo "java -Dwebdriver.gecko.driver=${FILE_GECKO} -Dwebdriver.chrome.driver=${FILE_CHROME} ${FILE}"
+java -jar -Dwebdriver.gecko.driver=${FILE_GECKO} -Dwebdriver.chrome.driver=${FILE_CHROME} ${FILE}
 
