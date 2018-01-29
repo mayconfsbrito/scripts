4234ad40b37d7f03dc367c68e783609ac44dd091
Maycon Brito <maycon.brito@bioextratus.rede.bioextratus>
2018-01-29 10:48:37 -0200

    Inserção de Comentários

--- git_set_remotes_origin.sh
+++ git_set_remotes_origin.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-#
+################################################################################
 # Percorre todos os repositórios GIT dentro de um diretório e remove todos os
 # seus remotos, substiuindo-os por um novo.
 #
+# Author: Maycon Brito
+################################################################################
+
+REMOTE='192.168.100.170'
+
 while IFS='' read -r -d '' filename; do
   cd $filename
   dir=$(printf '%s\n' "${PWD##*/}")
@@ -12,7 +17,7 @@ while IFS='' read -r -d '' filename; do
   echo 'Antes:'
   git remote -v
   git remote rm origin
-  git remote add origin ssh://git@192.168.100.170/repositorios/git/$dir
+  git remote add origin ssh://git@${REMOTE}/repositorios/git/$dir
 
   echo 'Depois:'
   git remote -v
